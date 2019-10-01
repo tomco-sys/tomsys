1c118f2f73080cd18613b3c79f69f5e8f47b826a
tomco-sys <mimikawa@mail.tomco.co.jp>
2019-10-01 17:58:43 +0900

    create company

--- ahobic/app/controllers/companies_controller.rb
+++ ahobic/app/controllers/companies_controller.rb
@@ -1,7 +1,11 @@
 class CompaniesController < ApplicationController
 
+  def index
+  end
+
   def create
     Company.create(company_name: com_params[:company_name], company_kana: com_params[:company_kana])
+    @companys = Company.all
   end
 
   private
