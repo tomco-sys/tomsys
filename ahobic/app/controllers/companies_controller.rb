030ebece140ece4efd3f61d2e3033818f4abc7b7
tomco-sys <mimikawa@mail.tomco.co.jp>
2019-10-01 16:29:44 +0900

    sqlite is warning

--- ahobic/app/controllers/companies_controller.rb
+++ ahobic/app/controllers/companies_controller.rb
@@ -1,2 +1,13 @@
 class CompaniesController < ApplicationController
+
+  def create
+    Company.create(company_name: com_params[:company_name], company_kana: com_params[:company_kana])
+  end
+
+  private
+
+  def com_params
+    params.permit(:company_name, :company_kana)
+  end
+
 end
